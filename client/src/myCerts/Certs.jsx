0f3291389734c7d52f93fd2c3c5faa7598e1fd7f
Luiz <l.guilherme128@gmail.com>
2024-02-27 06:48:11 -0300

    More certs

--- client/src/myCerts/Certs.jsx
+++ client/src/myCerts/Certs.jsx
@@ -3,6 +3,8 @@ const certs = [
   "https://firebasestorage.googleapis.com/v0/b/portfolio-8d5e6.appspot.com/o/UC-3189302e-06d6-4eaa-93b1-ae7b3a567ce4.jpg?alt=media&token=dd6e795f-4cad-41d0-b9da-ca1b2fef5637",
   "https://firebasestorage.googleapis.com/v0/b/portfolio-8d5e6.appspot.com/o/UC-4297df63-9252-4cab-8c3b-f500021e8f6c.jpg?alt=media&token=329ffdc2-c3ba-44ec-acd8-2a137a451663",
   "https://firebasestorage.googleapis.com/v0/b/portfolio-8d5e6.appspot.com/o/UC-7eabe3fc-8abb-4eb1-af83-072b371be9b4.jpg?alt=media&token=e93f7e78-cc6f-4ea8-913b-9031110e0b19",
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-8d5e6.appspot.com/o/Certificado%20Curso%20NR%2010%20-%20Luis%20Guilherme%20dos%20Anjos.pdf?alt=media&token=06e07e62-26d7-4a58-b68e-917e418216ae",
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-8d5e6.appspot.com/o/Certificado%20Curso%20NR%2035%20-%20Luis%20Guilherme%20dos%20Anjos.pdf?alt=media&token=01b1da01-e190-48c1-8990-a120d9b638ef",
 ];
 
 const Certs = () => {
